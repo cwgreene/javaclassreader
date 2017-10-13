5639f6a283a245efd02ff96a7e39565579ec5a2d
Chris Greene <archgoon@gmail.com>
2017-10-12 22:46:05 -0700

    Compiles, gets through constant pool.
    Lessons learned:
    
    Techniques learned for 'debugging' type errors:
      * Comment out most code
      * Get a compiled binary with most reduced set
      * Verify types of functions are what you expect using ghci
    
    Opportunity for tools (ideally from compiler itself):
      * Find the minimally compiling set of functions (via graph deps)
    
    For compiling code, this is easy, the trick is how to give useful
    information of compiling subsets of code.
    
    In a monad where you can do:
      x <- thing
      traceShow x (return x)
    to get good old printf debugging.
    
    Yes, haskell is in need of decent debugger.
    
    Mistakes made:
      * Got a "Non-exhaustive patterns in function getConstantPoolType"
        due to not reading word8 instead of word16
      * Improvements: Better handling of malformed class files.

--- jcr.hs
+++ jcr.hs
@@ -3,10 +3,19 @@ import Data.Binary.Get
 import Data.Word
 import qualified Data.ByteString.UTF8 as UTF8
 
-data Constant = ConstClass Int Int |
-    ConstFieldRef Int Int | ConstMethodRef Int Int | ConstInterfaceMethodRef Int Int |
-    ConstString Int | ConstInt Int | ConstFloat Int | ConstLong Int Int | ConstDouble Int Int |
-    ConstUtf8 String | ConstNameAndType Int Int | ConstMethodHandle Int Int | ConstInvokeDynamic Int Int
+import Debug.Trace
+
+data Constant = ConstClass Word16 |
+    ConstFieldRef Word16 Word16 |
+    ConstMethodRef Word16 Word16 |
+    ConstInterfaceMethodRef Word16 Word16 |
+    ConstString Word16 | ConstInt Word32 | ConstFloat Word32 |
+    ConstLong Word32 Word32 | ConstDouble Word32 Word32 |
+    ConstUtf8 String |
+    ConstNameAndType Word16 Word16 |
+    ConstMethodHandle Word8 Word16 |
+    ConstMethodType Word8 Word16 |
+    ConstInvokeDynamic Word16 Word16 deriving Show
 
 cCONSTANT_Class = 7
 cCONSTANT_Fieldref = 9
@@ -23,76 +32,74 @@ cCONSTANT_MethodHandle = 15
 cCONSTANT_MethodType = 16
 cCONSTANT_InvokeDynamic = 18
 
-getConstantPoolType :: Int -> Get Constant
-getConstantPoolType cCONSTANT_Class = do
-    name_index <- getWord16be
-    return (ConstClass name_index)
-
+getRef :: (Word16 -> Word16 -> Constant) -> Get Constant
 getRef constructor = do
-    class_index <- getWord8
-    name_and_type_index <- getWord8
-    return (constructor class_index name_and_type_index)
-
-getConstantPoolType cCONSTANT_Fieldref = getRef ConstFieldRef
-getConstantPoolType cCONSTANT_Methodref = getRef ConstMethodRef
-getConstantPoolType cCONSTANT_InterfaceMethodref =
-    getRef ConstInterfaceMethodRef
-
-getConstantPoolType cCONSTANT_String = do
-    string_index <- getWord16be
-    return (ConstString string_index)
+    class_index <- getWord16be
+    name_and_type_index <- getWord16be
+    return $ constructor class_index name_and_type_index
 
 getShortNum constructor = do
     bytes <- getWord32be
     return (constructor bytes)
 
-getConstantPoolType cCONSTANT_Integer = getShortNum ConstInt
-getConstantPoolType cCONSTANT_Float = getShortNum ConstFloat
-
-getLargeNum :: Int -> Int -> Constant -> Get Constant
+getLargeNum :: (Word32 -> Word32 -> Constant) -> Get Constant
 getLargeNum constructor = do
     high_bytes <- getWord32be
     low_bytes <- getWord32be
     return (constructor high_bytes low_bytes)
 
-getConstantPoolType cCONSTANT_Long = getLargeNum ConstLong
-getConstantPoolType cCONSTANT_Double = getLargeNum ConstDouble
-
-getConstantPoolType cCONSTANT_NameAndType = do
+getConstantPoolType :: Word16 -> Get Constant
+getConstantPoolType 7 = do
+    name_index <- getWord16be
+    return (ConstClass name_index)
+getConstantPoolType 9 = getRef ConstFieldRef
+getConstantPoolType 10 = getRef $ ConstMethodRef
+getConstantPoolType 11 =
+    getRef ConstInterfaceMethodRef
+getConstantPoolType 8 = do
+    string_index <- getWord16be
+    return (ConstString string_index)
+getConstantPoolType 3 = getShortNum ConstInt
+getConstantPoolType 4 = getShortNum ConstFloat
+getConstantPoolType 5 = getLargeNum ConstLong
+getConstantPoolType 6 = getLargeNum ConstDouble
+getConstantPoolType 12 = do
     name_index <- getWord16be
     descriptor_index <- getWord16be
     return (ConstNameAndType name_index descriptor_index)
-
-getConstantPoolType cCONSTANT_Utf8 = do
+getConstantPoolType 1 = do
     length <- getWord16be
-    bytes <- getByteString length
+    bytes <- getByteString (fromIntegral length)
     return (ConstUtf8 $ UTF8.toString bytes)
-
-getConstantPoolType cCONSTANT_MethodHandle = do
+getConstantPoolType 15 = do
     reference_kind <- getWord8
     reference_index <- getWord16be
-    return $ ConstMethodHandle $ reference_kind reference_index
-
-getConstantPoolType cCONSTANT_InvokeDynamic = do
+    return $ ConstMethodHandle reference_kind reference_index
+getConstantPoolType 16 = do
+    reference_kind <- getWord8
+    reference_index <- getWord16be
+    return $ ConstMethodType reference_kind reference_index
+getConstantPoolType 18 = do
     bootstrap_method_attr_index <- getWord16be
     name_and_type_index <- getWord16be
-    return $ ConstInvokeDynamic $ bootstrap_method_attr_index name_and_type_index
+    return $ ConstInvokeDynamic bootstrap_method_attr_index name_and_type_index
 
-
-getConstantPool 1 = []
+getConstantPool :: Int -> Get [Constant]
+getConstantPool 1 = return []
 getConstantPool x = do
     tag <- getWord8
-    info <- getConstantPoolType tag
-    return (info:(getConstantPool (x-1)))
+    info <- getConstantPoolType (fromIntegral tag)
+    next <- getConstantPool (x - 1)
+    return $ info:next
 
 readJCF = do
     magic <- getWord32be
     minor <- getWord16be
     major <- getWord16be
     constant_pool_count <- getWord16be
-    constant_pool <- getConstantPool constant_pool_count
+    constant_pool <- getConstantPool (fromIntegral constant_pool_count)
     return constant_pool
-{-    access_flags <- getWord16be
+    {-access_flags <- getWord16be
     this_class <- getWord16be
     super_class <- getWord16be
     interfaces_count <- getWord16be
